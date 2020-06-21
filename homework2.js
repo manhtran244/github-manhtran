6b4dbabae71e2a4bd996f15b3fd2c8a4c4443fa2
manhtran244 <66857598+manhtran244@users.noreply.github.com>
2020-06-21 21:02:54 +0700

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,186 @@
+//ex3
+//a.
+// for(let i = 0; i<7;i++){
+//     console.log(i)
+// }
+
+//b.
+// let a = parseInt(prompt('Nhap a:'))
+// for(let i = 0;i<a;i++){
+//     console.log(i)
+// }
+
+//c.
+// let b = parseInt(prompt('Nhap b:'));
+// for (let i = 3;i<b;i++){;
+//     console.log(i);
+// }
+
+//d.
+// let x = parseInt(prompt('Nhap x:'))
+// let y = parseInt(prompt('Nhap y:'))
+
+// for(let i =x;i<y;i++){
+//     console.log(i)
+// }
+
+//e.
+// let a = parseInt(prompt('Nhap a:'))
+// let b = parseInt(prompt('Nhap b:'))
+
+// for(let i = a; i<b;i+=3){
+//     console.log(i)
+// }
+
+// //f.
+// let a = parseInt(prompt('Nhap a:'))
+// let b = parseInt(prompt('Nhap b:'))
+// let c = parseInt(prompt('Nhap c:'))
+
+// for(let i = a; i<b; i+=c){
+//     console.log(i)
+// }
+
+//ex4.
+// let a =parseInt(prompt('Enter a number '))
+// console.log(a)
+// let result = 1
+// for( let i = 1; i <= a; i++){
+//     result*=i
+   
+// }
+// console.log('The factor of '+a+' is '+result)
+
+//ex5.
+// var a = prompt('How old are you?')
+// if(a>14){
+//     alert("Enjoy!")
+// }else{
+//    alert('You are not old enough to view this content')
+// }
+
+//ex6.
+// let a = parseInt(prompt('Enter a number'))
+// if(a<5){
+//     alert(a+ ' is lower half of 9 ')
+// }
+// if(a>9){
+//     alert('The number is not suitable')
+// }else{
+// alert(a+ ' is upper half of 9 ')
+// }
+
+//ex7.
+// let x = parseInt(prompt('x = '))
+// let y = parseInt(prompt('y = '))
+
+// if(x>(y/2)){
+//     alert(x + ' is upper half of '+ y)
+
+// }else{
+//     alert(x + ' is lower half of '+ y)
+// }
+
+// ex8.
+// let a = parseInt(prompt('a = '))
+// if(a%2==0){
+//     alert(a + ' is an even number')
+
+// }else{
+//     alert(a + ' is an odd number')
+// }
+
+//ex9.
+//a.
+// let a = 'L'
+// let b = 'H'
+
+// for(let i = 1;i<=3;i++){
+//     console.log(a)
+// }
+// for(let i = 1;i<=3;i++){
+//     console.log(b)
+// }
+
+//b.
+
+//c.
+// for(let i=0;i<4;i++){
+//     console.log(0)
+//     console.log(1)
+
+// }
+
+//d.
+// var a = prompt("Enter the number of 1's and 0's")
+// for(let i=0;i<(a/2);i++){
+//     console.log(0)
+//     console.log(1)
+// }
+
+
+//ex10.
+// let a = prompt('Your weight in kg');
+// let b = prompt('Your height in cm');
+// console.log(a)
+// console.log (b)
+// BMI = a/((b/100)*(b/100));
+// console.log( BMI)
+// alert(BMI)
+
+// if(BMI<16){
+//     alert('You are severely');
+//  }else if(BMI<18.5){
+//     alert('You are underweight');
+//  }else if(BMI<25){
+//      alert('you are normal');
+//  }else if(BMI<30){
+//      alert('You are overweight');
+//  }else{
+//      alert('You are obese');
+//  }
+
+//ex11.
+//a.
+// function square (){
+//     for(let i=1;i<=4;i++){
+//         fd(90)
+//         lt(90)
+//     }
+// }
+// square()
+
+//b.
+// function triangle() {
+//   for (var i=1; i <= 3; i++) {
+//     fd(90)
+//     lt(120)
+//        }
+// }
+// triangle()
+
+//c.
+// function pentagon(){
+//     for(let i =1;i<=5;i++){
+//       fd(90)
+//       lt(72)
+//     }
+//   }
+//   pentagon()
+
+//d.
+// function hexagon(){
+//   for(let i =1;i<=6;i++){
+//     fd(90)
+//     lt(60)
+//   }
+// }
+// hexagon()
+
+  let a = prompt('nhap so ')
+  if(a=3){
+      alert('asd')
+  }
+  else if(a=4){
+      alert('sfdsf')
+  }

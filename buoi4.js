bfcbd83fbfd852419ac4d2d48222749136e8d7f8
manhtran244 <66857598+manhtran244@users.noreply.github.com>
2020-06-28 21:15:30 +0700

    Add files via upload

---
+++ buoi4.js
@@ -0,0 +1,103 @@
+// let person1={
+//     name: 'Manh',
+//     age: 19,
+//     location: 'Ha Noi'
+// };
+
+// let person2 = {
+//     name: 'Tan',
+//     age: 15,
+//     loacation:'Yen Bai'
+// };
+
+// let personArr = [];
+
+// console.log(personArr);
+// personArr.push(person1);
+// personArr.push(person2);
+// console.log(personArr);
+
+// console.log(personArr[0]);
+
+// let p = personArr[0];
+// console.log(p);
+// console.log(p.name)
+
+// console.log(personArr[0].name);
+// let n = personArr[0].name
+// console.log(n);
+
+// for(let i =0;i<personArr.length;i++){
+// let p = (personArr[i]);
+// console.log(p);
+// console.log(p.age);
+// }
+
+
+// let film1 = {
+//     name: 'Attack on Titans',
+//     year: 2014,
+//     rate: 9.0,
+// };
+// let film2 = {
+//     name: 'Snow Piercer',
+//     year: 2020,
+//     rate: 8.2,
+// };
+// let film3 = {
+//     name: 'Dark',
+//     year: 2019,
+//     rate: 7.8,
+// };
+
+// let filmArr = [];
+// filmArr.push(film1);
+// filmArr.push(film2);
+// filmArr.push(film3);
+
+// console.log(filmArr);
+
+// for(let i =0;i<filmArr.length;i++){
+//    let a =filmArr[i]
+//     console.log(a);
+//     console.log(a.name);
+//     console.log(a.rate);
+//     console.log(a.year);
+// }
+    // console.log(film1);
+// console.log(film3.name);
+// }
+
+
+// let person = {
+//     name:'Nam',
+//     age: 19,
+//     favs: ['Netflix', 'Coffee','Code']
+// };
+// // console.log(person);
+// // person.favs.push('Facebook')
+// // console.log(person);
+// // console.log(person.favs[1])
+//  let favs = person.favs;
+//  console.log(favs);
+//  for(let i =0;i<favs.length;i++){
+//      console.log(favs[i]);
+//  }
+// favs.push('Facebook');
+// console.log(person);
+
+//containing Array ex
+
+// let film = {
+//     name: 'Attack on Titans',
+//     year: 2014,
+//     rate:9.0,
+//     review:['great intro music', 'great plot twist',' happy ending']
+// };
+
+// console.log(film.name);
+// console.log(film.year);
+// console.log(film.rate);
+// console.log(film.review);
+
+
